aefd7719f429487e67b1c3b3c7a63ef691d68a55
maheshashinde <121862667+maheshashinde@users.noreply.github.com>
2023-02-18 14:31:36 +0530

    Update CodeqlDemoApplicationTests.java

--- src/test/java/com/example/codeqldemo/CodeqlDemoApplicationTests.java
+++ src/test/java/com/example/codeqldemo/CodeqlDemoApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.codeqldemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CodeqlDemoApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
