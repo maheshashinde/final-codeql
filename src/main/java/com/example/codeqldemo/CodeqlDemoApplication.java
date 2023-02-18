c8fb036b10608dcaa531dd5439d7b9ead9f62dfc
maheshashinde <121862667+maheshashinde@users.noreply.github.com>
2023-02-18 15:47:10 +0530

    Update CodeqlDemoApplication.java

--- src/main/java/com/example/codeqldemo/CodeqlDemoApplication.java
+++ src/main/java/com/example/codeqldemo/CodeqlDemoApplication.java
@@ -1,36 +1,46 @@
 package com.example.codeqldemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.sql.*;
+
+@SpringBootApplication
 public class CodeqlDemoApplication {
 
 	public static void main(String[] args) {
-		try {
-			// Connect to the database
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "username", "password");
+		SpringApplication.run(CodeqlDemoApplication.class, args);
 
-			// Get user input
-			String username = args[0];
-
-			// Construct SQL query
-			String sql = "SELECT * FROM users WHERE username = '" + username + "'";
-
-			// Execute query
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-
-			// Process results
-			while (rs.next()) {
-				String resultUsername = rs.getString("username");
-				String resultPassword = rs.getString("password");
-				System.out.println("Username: " + resultUsername + ", Password: " + resultPassword);
-			}
-
-			// Clean up
-			rs.close();
-			stmt.close();
-			conn.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 	}
+//	public static void main(String[] args) {
+//		sp
+//		try {
+//			// Connect to the database
+//			Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "username", "password");
+//
+//			// Get user input
+//			String username = args[0];
+//
+//			// Construct SQL query
+//			String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+//
+//			// Execute query
+//			Statement stmt = conn.createStatement();
+//			ResultSet rs = stmt.executeQuery(sql);
+//
+//			// Process results
+//			while (rs.next()) {
+//				String resultUsername = rs.getString("username");
+//				String resultPassword = rs.getString("password");
+//				System.out.println("Username: " + resultUsername + ", Password: " + resultPassword);
+//			}
+//
+//			// Clean up
+//			rs.close();
+//			stmt.close();
+//			conn.close();
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
